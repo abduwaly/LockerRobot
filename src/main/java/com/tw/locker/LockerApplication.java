8648e775b1f18a463b4f98694517acb1ad0ad1da
abduwaly <arsay@foxmail.com>
2020-09-15 21:00:59 +0800

    initialize project using sprintboot

---
+++ src/main/java/com/tw/locker/LockerApplication.java
@@ -0,0 +1,13 @@
+package com.tw.locker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockerApplication.class, args);
+	}
+
+}
